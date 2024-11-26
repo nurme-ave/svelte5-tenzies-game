e394b0a6b17033f3f7c1f0f7e7ecb0fe58913716
Ave Nurme <nurme.ave@gmail.com>
2024-11-27 01:08:36 +0200

    Replace "typeof window !== 'undefined'" with 'browser' to align more with SvelteKit's conventions and which is easier to read

--- src/lib/stores/audioStore.js
+++ src/lib/stores/audioStore.js
@@ -1,4 +1,6 @@
+// src\lib\stores\audioStore.js
 import { writable } from 'svelte/store';
+import { browser } from '$app/environment';
 
 // Create the store with a default value
 const audioStore = writable({
@@ -6,7 +8,7 @@ const audioStore = writable({
 });
 
 // Initialize the store with localStorage value if we're in the browser
-if (typeof window !== 'undefined') {
+if (browser) {
   const storedMute = localStorage.getItem('tenziesAppMuted');
   if (storedMute !== null) {
     audioStore.set({ isMuted: JSON.parse(storedMute) });
@@ -18,7 +20,7 @@ export function toggleMute() {
   audioStore.update((state) => {
     const newState = { ...state, isMuted: !state.isMuted };
     // Only access localStorage in the browser
-    if (typeof window !== 'undefined') {
+    if (browser) {
       localStorage.setItem('tenziesAppMuted', JSON.stringify(newState.isMuted));
     }
     return newState;
